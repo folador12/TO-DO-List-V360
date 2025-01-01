72275aa38efd1743fa23db9d6568e30a63c3d66e
folador12 <luizantoniofoladorfilho@gmail.com>
2025-01-01 16:38:53 -0300

    feat: adding colors to category and styling lists with category colors

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :lists
 
   validates :name, presence: true
+  validates :color, format: { with: /\A#[0-9A-Fa-f]{6}\z/, message: "deve ser um código hexadecimal válido (e.g., #FF5733)" }, allow_blank: true
 end
